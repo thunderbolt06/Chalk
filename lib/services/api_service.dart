50b4efc01709e11acd7c71fed4f5018f632e079e
Rahil Jain <f20170541@pilani.bits-pilani.ac.in>
2023-09-30 14:35:06 +0530

    connected to backend

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 Future<String> fetchHelloWorld() async {
-  final response = await http.get(Uri.parse('http://127.0.0.1:8000/api/hello'));
+  final response =
+      await http.get(Uri.parse('https://chalkapi.azurewebsites.net/api/hello'));
 
   if (response.statusCode == 200) {
     // Request successful, parse the response data
